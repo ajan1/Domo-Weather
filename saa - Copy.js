96eebbb1899daef24aee71e81402934057c98573
lauritus <lauri.nuuttinen@gmail.com>
2014-03-12 15:56:09 +0200

    Remove old file

--- saa - Copy.js
+++
@@ -1,8 +0,0 @@
-var request = require('request');
-var saa = ("");
-var response = request.get('http://api.openweathermap.org/data/2.5/weather?q=Helsinki,FI&units=metric&lang=fi', function (error, response, body) {
-  if (!error && response.statusCode == 200) {
-    var response = JSON.parse(body);
-    var saa = (response.main.temp);
-  }
-});
